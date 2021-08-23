17676ffe97863cf4bad93ad55bc3c1902bf20263
TMichael <js5859168@gmail.com>
2021-08-23 12:14:43 +0300

    Lesson1 home work

---
+++ src/AdsLesson1/Main.java
@@ -0,0 +1,13 @@
+package AdsLesson1;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
